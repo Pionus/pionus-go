ccd4b3eebcec3f49cab584b4a862a0298a4b45d7
Secbone <secbone@gmail.com>
2019-05-19 15:08:21 +0800

    update: framework to arry

--- server.go
+++ server.go
@@ -4,8 +4,8 @@ import (
     "net/http"
     "fmt"
     "log"
-    "github.com/pionus/framework"
-    "github.com/pionus/framework/middlewares"
+    "github.com/pionus/arry"
+    "github.com/pionus/arry/middlewares"
 )
 
 
@@ -13,7 +13,7 @@ import (
 func main() {
     config := GetConfig()
 
-	app := pionus.NewApp()
+	app := arry.New()
     app.Use(middlewares.Logger)
     app.Use(middlewares.Panic)
 
@@ -21,20 +21,20 @@ func main() {
 
     router := app.Router()
 
-    router.Get("/", func(ctx *pionus.Context) {
+    router.Get("/", func(ctx arry.Context) {
 		ctx.Text(http.StatusOK, "hey~")
 	})
 
-	router.Get(`/hello`, func(ctx *pionus.Context) {
+	router.Get(`/hello`, func(ctx arry.Context) {
 		ctx.Text(http.StatusOK, "Hello world")
 	})
 
-	router.Get(`/hello/:name`, func(ctx *pionus.Context) {
-        fmt.Printf("hello %s", ctx.Params["name"])
-		ctx.Text(http.StatusOK, fmt.Sprintf("Hello %s", ctx.Params["name"]))
+	router.Get(`/hello/:name`, func(ctx arry.Context) {
+        fmt.Printf("hello %s", ctx.Param("name"))
+		ctx.Text(http.StatusOK, fmt.Sprintf("Hello %s", ctx.Param("name")))
 	})
 
-    router.Get("/push", func(ctx *pionus.Context) {
+    router.Get("/push", func(ctx arry.Context) {
         ctx.Push("/static/article.css")
         ctx.Push("/static/article.js")
         ctx.Text(http.StatusOK, "pushed~")
